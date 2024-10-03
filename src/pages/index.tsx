3a1629006a1006dc51be0927cadc1b124cce2fb9
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2024-10-03 08:47:38 +0700

    add aceternityui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,31 @@
 import { RainbowButton } from "@/components/ui/rainbow-button";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 import Image from "next/image";
 
 export default function Home() {
-  return <RainbowButton>Get Unlimited Access</RainbowButton>;
+  return (
+    <>
+      <RainbowButton>Get Unlimited Access</RainbowButton>
+      <TypewriterEffectSmooth
+        words={[
+          {
+            text: "Build",
+          },
+          {
+            text: "awesome",
+          },
+          {
+            text: "apps",
+          },
+          {
+            text: "with",
+          },
+          {
+            text: "Aceternity.",
+            className: "text-blue-500 dark:text-blue-500",
+          },
+        ]}
+      />
+    </>
+  );
 }
